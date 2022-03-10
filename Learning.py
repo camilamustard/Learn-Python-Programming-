e27163d64aad1b6a08cb3b611bc2b6219492105c
Camila Mustard <86597544+camilamustard@users.noreply.github.com>
2022-03-10 09:28:21 -0300

    Add files via upload

---
+++ Learning.py
@@ -0,0 +1,38 @@
+class Calculator:
+    def __init__(self):
+        self.current = 0
+        self.total = 0
+
+    def add(self, amount):
+        self.current += amount
+
+    def get_current(self):
+        return self.current
+    x= "Pluto is a planet"
+    y= "Pluto is a planet"
+        print ("Pluto's a planet")
+#Algorítimo: 1 Perguntar ao cliente quantas pizzas ele gostaria de adquirir.
+    numero_pizzas = int (input ('Quantas pizzas você gostaria?'))
+#Algoritimo 2: Informar ao cliente que ele fez uma ótima escolha e pedir pra esperar enquanto o pedido é calculado.
+    print('\nExcelente escolha! Agora deixe-me calcular o valor total do seu pedido')
+#Algoritmo 3; Calcular preço do pedido. Valor da pizza 30 reais
+    total = numero_pizzas*30.0
+#Algoritmo 4 informar ao cliente o preço do pedido
+    print(O preço total do seu pedido é R$ total, '.', sep="")
+
+     preço_pizza=30.0
+     número_pizzas- 10
+     total = preço_pizza*numero_pizzas
+80   n
+spam_amount=0
+print(spam_amount)
+#ordering spam egg, spam, bacon and spam
+
+if spam_amount>0
+    print("but i don't want any spam!")
+
+viking_song= "Spam" * spam_amount
+print(viking_song)
+
+spam_amount=0
+
