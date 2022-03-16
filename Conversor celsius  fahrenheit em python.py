d5ba6a458d7abecf83c924ee511c98285e958e11
Camila Mustard <86597544+camilamustard@users.noreply.github.com>
2022-03-16 10:21:27 -0300

    Add files via upload
    Projeto de calculadora conversor de temperatura Celsius para  fahrenheit.

---
+++ Conversor celsius  fahrenheit em python.py
@@ -0,0 +1,7 @@
+
+temp_C= float (input('digite a temperatura em C:'))
+temp_F= temp_C*9/5 +32
+print('A temperatura em fahrenheit é de: ', temp_F,"F", sep="")
+temp_C= float (input('digite a temperatura em C:'))
+temp_F= temp_C*9/5 +32
+print('A temperatura em fahrenheit é de: ', temp_F,"F", sep="")
